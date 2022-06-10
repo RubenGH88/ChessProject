2761839705560b6b1b09cbcaf2bc9e0348fcf2aa
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-06-10 19:19:42 +0200

    Project Chess
    The pieces are moving
    only logic its missing

--- src/index.js
+++ src/index.js
@@ -8,28 +8,64 @@ pieces.forEach((piece)=>{piece.draw()})
 
 }
 
-let movement=(e)=>{
-//piece i want to move
-let selected=["D",2];
-let pieceSelectedArray = pieces.filter((piece)=>{return piece.column===selected[0]&&piece.row===selected[1]})
-let pieceSelected=pieceSelectedArray[0]
 
-//where i want to go
-let destiny=["D",4]
-pieceSelected.destiny=destiny
-//order to move
-pieceSelected.move()
-console.log(e)
-console.log(locateRow(e))
-console.log(locateColumn(e))
-canvas.addEventListener("click",movement);
+
+let originRow=0
+let originColumn
+let destinyRow
+let destinyColumn
+
+let pieceSelected
+
+
+let onclick =(e)=>{
+    if(originRow!==0){
+      console.log("Aqui si tengo origin")
+        
+        //where i want to go
+        destinyRow=locateRow(e)
+        destinyColumn=locateColumn(e)
+        
+        let destiny=[destinyRow,destinyColumn]
+     
+        pieceSelected.destiny=destiny
+       
+        
+        pieceSelected.move()
+        
+        console.log(originRow)    
+     
+        originRow=0;
+        originColumn=undefined
+        destinyRow=undefined
+        destinyColumn=undefined
+         
+        
+
+       
+
+    }
+    
+    else if(originRow===0)
+    {console.log(originRow)
+    {console.log("aqui no tengo origin")
+        originRow=locateRow(e)
+        originColumn=locateColumn(e)
+        
+        let selected=[originRow,originColumn];
+        
+        let pieceSelectedArray = pieces.filter((piece)=>{return piece.column===selected[0]&&piece.row===selected[1]})
+       
+        pieceSelected=pieceSelectedArray[0]
+       console.log("selecciono pieza")
+       console.log(originRow)
+       console.log(originColumn)}
+    }
+    
+
+    
 
 }
+canvas.addEventListener("click",(e)=>{onclick(e)});
 
-
-canvas.addEventListener("click",(e)=>{locateRow(e)});
-
-canvas.addEventListener("click",(e)=>{locateColumn(e)});
-//canvas.addEventListener("mouseDown",movement);
-canvas.addEventListener("click",movement);
 window.addEventListener("load",setup);
